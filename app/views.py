eb8efc4dbb8feabaa159f4e889921c97f0852b35
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-04-05 11:44:18 +0300

    fixed pagination, added components

--- app/views.py
+++ app/views.py
@@ -11,6 +11,7 @@ QUESTIONS = [
         "id": i,
         "title": f"Question {i}",
         "text": f"This is question number {i}",
+        "likes": randint(0,10),
         "tags": [
             f"tag{randint(0,10)}",
             f"tag{randint(0,10)}",
@@ -27,16 +28,16 @@ QUESTIONS = [
 ]
 
 def index(request):
-    return render(request, "index.html", {"questions": paginate(QUESTIONS, request)})
+    return render(request, "index.html", {"questions": paginate(QUESTIONS, request, per_page=20)})
 
 def hot_questions(request):
-    return render(request, "hot_questions.html", {"questions": paginate(QUESTIONS, request)})
+    return render(request, "hot_questions.html", {"questions": paginate(QUESTIONS, request, per_page=20)})
 
 def settings(request):
     return render(request, "settings.html")
 
 def logout(request):
-    return render(request, "index.html", {"questions": paginate(QUESTIONS, request)})
+    return render(request, "index.html", {"questions": paginate(QUESTIONS, request, per_page=20)})
 
 def tag(request, tag):
     questions = []
@@ -44,7 +45,7 @@ def tag(request, tag):
         if tag in QUESTIONS[i].get("tags"):
             questions.append(QUESTIONS[i])
     return render(request, "tag.html", {
-        "questions": paginate(questions, request),
+        "questions": paginate(questions, request, per_page=20),
         "tag": tag,
         }
     )
@@ -55,7 +56,7 @@ def ask(request):
 def question(request, question_id):
     return render(request, "question.html", {
         "question": QUESTIONS[int(question_id)],
-        "answers": paginate(QUESTIONS[int(question_id)].get("answers"), request),
+        "answers": paginate(QUESTIONS[int(question_id)].get("answers"), request, per_page=30),
         }
     )
 
@@ -65,8 +66,16 @@ def login(request):
 def signup(request):
     return render(request, "signup.html")
 
-def paginate(obj_list, request, per_page=5):
+def paginate(obj_list, request, per_page=10):
     page_num = request.GET.get("page", 1)
     paginator = Paginator(obj_list, per_page)
-    page_obj = paginator.page(page_num)
+    try:
+        page_num = int(page_num)
+    except:
+        page_obj = paginator.page(1)
+        return page_obj
+    if page_num > 0:
+        page_obj = paginator.page(page_num)
+    else:
+        page_obj = paginator.page(1)
     return page_obj
