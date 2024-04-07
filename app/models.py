d2c59796171eecc02171b600009b7cc937d83791
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-04-07 10:08:47 +0300

    task 3 in progress

--- app/models.py
+++ app/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+# Пользователь – электронная почта, никнейм, пароль, аватарка, дата регистрации, рейтинг.
+# Вопрос – заголовок, содержание, автор, дата создания, теги, рейтинг.
+# Ответ – содержание, автор, дата написания, флаг правильного ответа, рейтинг.
+# Тег – слово тега.
+
+class Tags(models.Model):
+    name = models.CharField(max_length=255)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    nickname = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    avatar = models.ImageField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    author = models.ForeignKey(Profile)
+    tags = models.ManyToManyField(Tags)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class QuestionsRating(models.Model):
+    VALUE_CHOICES = [
+        (1, 1),
+        (-1, -1),
+    ]
+    value = models.SmallIntegerField(choices=VALUE_CHOICES)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+class Answer(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(Profile)
+    created_ar = models.DateTimeField(auto_now_add=True)
+
+class AnswersRating(models.Model):
+    VALUE_CHOICES = [
+        (1, 1),
+        (-1, -1),
+    ]
+    value = models.SmallIntegerField(choices=VALUE_CHOICES)
+    answer = models.ForeignKey(Answer)
+    author = models.ForeignKey(Profile)
--- askme_novikov/settings.py
+++ askme_novikov/settings.py
@@ -76,10 +76,17 @@ WSGI_APPLICATION = 'askme_novikov.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# import pymysql
+# pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
